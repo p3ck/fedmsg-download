7895eded5c41505873de9270f1ec1c41e81df704
Bill Peck <bpeck@redhat.com>
2012-10-24 14:21:11 -0400

    Initial Commit

---
+++ fedmsg_download/consumer.py
@@ -0,0 +1,51 @@
+# This file is part of fedmsg-downloader.
+# Copyright (C) 2012 Red Hat, Inc.
+#
+# fedmsg-downloader is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# fedmsg-downloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with fedmsg; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Authors:  Bill Peck <bpeck@redhat.com>
+#
+import fedmsg
+import re
+from fedmsg.consumers import FedmsgConsumer
+
+import logging
+log = logging.getLogger("moksha.hub")
+
+
+class RsyncConsumer(FedmsgConsumer):
+    topic = "org.fedoraproject.compose"
+    config_key = 'fedmsg-download.consumer.enabled'
+
+    def __init__(self, hub):
+        self.hub = hub
+        self.DBSession = None
+
+        settings = hub.config.get('download')
+        self.regex_topic = re.compile(settings.get('filter_topic', ''))
+
+        return super(RsyncConsumer, self).__init__(hub)
+
+    def consume(self, msg):
+        """ Look for topic messages that match the config
+            branched.rsync.complete
+            rawhide.rsync.complete
+        """
+        log.info("Duhhhh... got: %r" % msg)
+        topic = msg['body']['topic']
+        if self.regex_topic.search(topic):
+            branch = msg['body']['msg'].get('branch', None)
+            log.info("branch: %s" % branch)
+            # sync it down...
---
+++ setup.py
@@ -0,0 +1,94 @@
+# This file is part of fedmsg.
+# Copyright (C) 2012 Red Hat, Inc.
+#
+# fedmsg is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# fedmsg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with fedmsg; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Authors:  Ralph Bean <rbean@redhat.com>
+#
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+import sys
+
+# Ridiculous as it may seem, we need to import multiprocessing and
+# logging here in order to get tests to pass smoothly on python 2.7.
+try:
+    import multiprocessing
+    import logging
+except Exception:
+    pass
+
+
+install_requires = [
+    'pyzmq',
+    'fabulous',
+    'kitchen',
+    'moksha.hub',
+    'requests',
+    'pygments',
+    'fedmsg',
+    #'daemon',
+
+    # These are "optional" for now to make installation from pypi easier.
+    #'M2Crypto',
+    #'m2ext',
+]
+tests_require = [
+    'nose',
+]
+
+if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
+    install_requires.extend([
+        'argparse',
+        'ordereddict',
+    ])
+    tests_require.extend([
+        'unittest2',
+    ])
+
+
+setup(
+    name='fedmsg-download',
+    version='0.1.2',
+    description="Fedora Messaging Downloading Consumer",
+    long_description="Fedora Messaging Downloading Consumer",
+    author='Bill Peck',
+    author_email='bpeck@redhat.com',
+    url='http://github.com/bpeck/fedmsg-download/',
+    license='LGPLv2+',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    packages=[
+        'fedmsg_download',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            "fedmsg-download=fedmsg_download.command:download",
+        ],
+        'moksha.consumer': [
+            "downloader=fedmsg_download.consumer:RsyncConsumer",
+        ],
+        'moksha.producer': [
+        ],
+    }
+)
